72e92b7584a04924e2d50ce961da35d1057ed62c
mateuszsoltys99-spec <mateusz.soltys99@gmail.com>
2026-03-07 12:52:32 +0100

    Added firewall options config

---
+++ proxmox/config_node_firewall_options.go
@@ -0,0 +1,147 @@
+package proxmox
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+)
+
+type NodeFirewallOptions struct {
+	Enable                           bool   `json:"enable,omitempty"`
+	LogLevelForward                  string `json:"log_level_forward,omitempty"`
+	LogLevelIn                       string `json:"log_level_in,omitempty"`
+	LogLevelOut                      string `json:"log_level_out,omitempty"`
+	LogNfConntrack                   bool   `json:"log_nf_conntrack,omitempty"`
+	Ndp                              bool   `json:"ndp,omitempty"`
+	NfConntrackAllowInvalid          bool   `json:"nf_conntrack_allow_invalid,omitempty"`
+	NfConntrackHelpers               string `json:"nf_conntrack_helpers,omitempty"`
+	NfConntrackMax                   int    `json:"nf_conntrack_max,omitempty"`
+	NfConntrackTcpTimeoutEstablished int    `json:"nf_conntrack_tcp_timeout_established,omitempty"`
+	NfConntrackTcpTimeoutSynRecv     int    `json:"nf_conntrack_tcp_timeout_syn_recv,omitempty"`
+	Nftables                         bool   `json:"nftables,omitempty"`
+	Nosmurfs                         bool   `json:"nosmurfs,omitempty"`
+	ProtectionSynflood               bool   `json:"protection_synflood,omitempty"`
+	ProtectionSynfloodBurst          int    `json:"protection_synflood_burst,omitempty"`
+	ProtectionSynfloodRate           int    `json:"protection_synflood_rate,omitempty"`
+	SmurfLogLevel                    string `json:"smurf_log_level,omitempty"`
+	TcpFlagsLogLevel                 string `json:"tcp_flags_log_level,omitempty"`
+	Tcpflags                         bool   `json:"tcpflags,omitempty"`
+}
+
+func NewNodeFirewallOptions() NodeFirewallOptions {
+	return NodeFirewallOptions{}
+}
+
+func NewNodeFirewallOptionsFromJson(input []byte) (firewallOptions NodeFirewallOptions, err error) {
+	firewallOptions = NewNodeFirewallOptions()
+	err = json.Unmarshal([]byte(input), &firewallOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func NewNodeFirewallOptionsFromAPICall(ctx context.Context, node string, client *Client) (firewallOptions *NodeFirewallOptions, err error) {
+	retrievedFirewallRule, err := client.ReadNodeFirewallOptions(ctx, node)
+	if err != nil {
+		return nil, err
+	}
+	return mapToNodeFirewallOptions(retrievedFirewallRule), nil
+}
+
+func (nodeFirewallOptions *NodeFirewallOptions) UpdateNodeFirewallOptions(ctx context.Context, node string, client *Client) error {
+	return client.UpdateNodeFirewallOptions(ctx, node, nodeFirewallOptionsToMap(*nodeFirewallOptions))
+}
+
+func getString(m map[string]interface{}, key string) string {
+	if v, ok := m[key]; ok {
+		if s, ok := v.(string); ok {
+			return s
+		}
+	}
+	return ""
+}
+
+func getBool(m map[string]interface{}, key string) bool {
+	if v, ok := m[key]; ok {
+		if b, ok := v.(bool); ok {
+			return b
+		}
+	}
+	return false
+}
+
+func getInt(m map[string]interface{}, key string) int {
+	if v, ok := m[key]; ok {
+		switch val := v.(type) {
+		case int:
+			return val
+		case float64: // common when coming from json.Unmarshal
+			return int(val)
+		}
+	}
+	return 0
+}
+
+func mapToNodeFirewallOptions(input map[string]interface{}) *NodeFirewallOptions {
+	return &NodeFirewallOptions{
+		Enable:                           getBool(input, "enable"),
+		LogLevelForward:                  getString(input, "log_level_forward"),
+		LogLevelIn:                       getString(input, "log_level_in"),
+		LogLevelOut:                      getString(input, "log_level_out"),
+		LogNfConntrack:                   getBool(input, "log_nf_conntrack"),
+		Ndp:                              getBool(input, "ndp"),
+		NfConntrackAllowInvalid:          getBool(input, "nf_conntrack_allow_invalid"),
+		NfConntrackHelpers:               getString(input, "nf_conntrack_helpers"),
+		NfConntrackMax:                   getInt(input, "nf_conntrack_max"),
+		NfConntrackTcpTimeoutEstablished: getInt(input, "nf_conntrack_tcp_timeout_established"),
+		NfConntrackTcpTimeoutSynRecv:     getInt(input, "nf_conntrack_tcp_timeout_syn_recv"),
+		Nftables:                         getBool(input, "nftables"),
+		Nosmurfs:                         getBool(input, "nosmurfs"),
+		ProtectionSynflood:               getBool(input, "protection_synflood"),
+		ProtectionSynfloodBurst:          getInt(input, "protection_synflood_burst"),
+		ProtectionSynfloodRate:           getInt(input, "protection_synflood_rate"),
+		SmurfLogLevel:                    getString(input, "smurf_log_level"),
+		TcpFlagsLogLevel:                 getString(input, "tcp_flags_log_level"),
+		Tcpflags:                         getBool(input, "tcpflags"),
+	}
+}
+
+func nodeFirewallOptionsToMap(opts NodeFirewallOptions) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	result["enable"] = opts.Enable
+	result["log_nf_conntrack"] = opts.LogNfConntrack
+	result["ndp"] = opts.Ndp
+	result["nf_conntrack_allow_invalid"] = opts.NfConntrackAllowInvalid
+	result["nf_conntrack_max"] = opts.NfConntrackMax
+	result["nf_conntrack_tcp_timeout_established"] = opts.NfConntrackTcpTimeoutEstablished
+	result["nf_conntrack_tcp_timeout_syn_recv"] = opts.NfConntrackTcpTimeoutSynRecv
+	result["nftables"] = opts.Nftables
+	result["nosmurfs"] = opts.Nosmurfs
+	result["protection_synflood"] = opts.ProtectionSynflood
+	result["protection_synflood_burst"] = opts.ProtectionSynfloodBurst
+	result["protection_synflood_rate"] = opts.ProtectionSynfloodRate
+	result["tcpflags"] = opts.Tcpflags
+
+	if opts.LogLevelForward != "" {
+		result["log_level_forward"] = opts.LogLevelForward
+	}
+	if opts.LogLevelIn != "" {
+		result["log_level_in"] = opts.LogLevelIn
+	}
+	if opts.LogLevelOut != "" {
+		result["log_level_out"] = opts.LogLevelOut
+	}
+	if opts.NfConntrackHelpers != "" {
+		result["nf_conntrack_helpers"] = opts.NfConntrackHelpers
+	}
+	if opts.SmurfLogLevel != "" {
+		result["smurf_log_level"] = opts.SmurfLogLevel
+	}
+	if opts.TcpFlagsLogLevel != "" {
+		result["tcp_flags_log_level"] = opts.TcpFlagsLogLevel
+	}
+
+	return result
+}
